37ba79408ee9cff15f95f93d3049a36d601786e6
ayninur <nuralaynabdulqadir@gmail.com>
2020-12-02 17:44:38 -0500

    jim and dwights turns are storing into an array of ints

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // select cells
 const cells = document.querySelectorAll('.cell');
+const board = document.getElementById('board');
 
 //turn
 let turn
@@ -8,19 +9,47 @@ let turn
 const dwight = `<img src="images/dwight.png" height="200px">`;
 const jim = `<img src="images/jim.png" height="200px">`
 
-//add event listener to each cell
-cells.forEach(cell => {
-    cell.addEventListener('click', handleClick, { once: true }) //only does click event once
-})
+// WINNING COMBOS
+const wins = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
 
-function handleClick(e) {
-    if (turn = !turn) {
-        e.target.innerHTML = jim
-    } else {
-        e.target.innerHTML = dwight
+//store jims choices
+let jimsChoices = []
+
+//store dwights choices
+let dwightsChoices = []
+
+
+
+startGame()
+
+function startGame() {
+    //add event listener to each cell
+    cells.forEach(cell => {
+        cell.addEventListener('click', handleClick, { once: true }) //only does click event once
+    })
+
+    //switches turns
+    function handleClick(e) {
+        if (turn = !turn) {
+            e.target.innerHTML = jim
+            jimsChoices.push(+e.target.id) //push id as int
+        } else {
+            e.target.innerHTML = dwight
+            dwightsChoices.push(+e.target.id)
+        }
     }
+
+
 }
-    // place mark
     // check for win
     // check for draw
     // switch turns
